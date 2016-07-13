3afb57ceaa770bb1210406b6d58078a8ca11502d
Gray Reinhard <gray.reinhard@gmail.com>
2016-07-13 14:30:51 -0400

    problem 17 & 18 in js

--- eulers.js
+++ eulers.js
@@ -293,4 +293,54 @@ function problem16() {
 	return powerDigitSum(1000);
 }
 
-console.log(problem16())
+// 17) Number letter counts
+
+function numberLetterCounts(num) {
+	var total = 0;
+	for (i = 1; i <= num; i++) {
+		str = writtenNumber(i).replace(/[\s\-]+/g, '');
+		total += str.length;
+	}
+	return total;
+}
+
+function problem17() {
+	return numberLetterCounts(1000);
+}
+
+// 18) Maximum path sum I
+
+function maxPathSumI(array) {
+	var arrayDup = array;
+	for (var i = array.length - 1; i >= 0; i--) {
+		for (var x = 0; x < array[i].length - 1; x++) {
+			if (i > 0) {
+				arrayDup[i - 1][x] = arrayDup[i][x] >= arrayDup[i][x + 1] ? arrayDup[i - 1][x] + arrayDup[i][x] : arrayDup[i - 1][x] + arrayDup[i][x + 1];
+			}
+		}
+	};
+	return arrayDup[0][0];
+}
+
+function problem18() {
+	var array = [
+		[75],
+		[95, 64],
+		[17, 47, 82],
+		[18, 35, 87, 10],
+		[20, 04, 82, 47, 65],
+		[19, 01, 23, 75, 03, 34],
+		[88, 02, 77, 73, 07, 63, 67],
+		[99, 65, 04, 28, 06, 16, 70, 92],
+		[41, 41, 26, 56, 83, 40, 80, 70, 33],
+		[41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+		[53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+		[70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+		[91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+		[63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+		[04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23]
+	];
+	return maxPathSumI(array);
+}
+
+console.log(problem18())
